61c41362102221672f9a159c1137415bede16c9f
SOUnit <workbookwork7@gmail.com>
2021-03-27 15:59:56 +0900

    added comment to problem sample

--- src/actions/memorize_problem_sample/index.js
+++ src/actions/memorize_problem_sample/index.js
@@ -11,7 +11,14 @@ export const fetchPosts = () => async (dispatch) => {
 //   dispatch({ type: 'FETCH_USER', payload: response.data });
 // };
 
+// ============================================================
+// the code below is for dispalying problem of memoize
+// they dont work as we expect
+// they request multiple times still even though memorize
+// ===========================================================
+
 // memorize not work well 1
+// this returns inner function, then http request is done
 // export const fetchUser = _.memoize(function (id) {
 //   return async function (dispatch) {
 //     const response = await jsonPlaceholder.get(`/users/${id}`);
@@ -20,6 +27,8 @@ export const fetchPosts = () => async (dispatch) => {
 // });
 
 // memorize not work well 2
+// this memorize inner function, but inner function is new everytime.
+// so they make http request everytime
 export const fetchUser = function (id) {
   return _.memoize(async function (dispatch) {
     const response = await jsonPlaceholder.get(`/users/${id}`);
